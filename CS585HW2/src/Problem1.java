e4c6fc0b7f7fc6d65fc8a5a035467abb72ece843
bl75d <AAbb11!!>
2020-10-08 05:15:55 +0800

    HW2Problem1

---
+++ CS585HW2/src/Problem1.java
@@ -0,0 +1,60 @@
+import java.util.Random;
+import java.lang.StringBuilder;
+import java.awt.Point;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintStream;
+//These parameters can only generate 10000 data of points and rectangels, the size of output data file is around 100K
+class Data {
+	static class points {
+		int x;
+		int y;
+		int boundry = 10000;
+
+		public void create_point() {
+			Random rp = new Random();
+			x = rp.nextInt(boundry);
+			y = rp.nextInt(boundry);
+		}
+	}
+
+	static class rectangle {
+		int bot_leftx;
+		int bot_lefty;
+		int w;
+		int h;
+		int boundry = 10000;
+		int w_boundry = 5;
+		int h_boundry = 20;
+
+		public void create_rec() {
+			Random rc = new Random();
+			bot_leftx = rc.nextInt(boundry - w_boundry);
+			bot_lefty = rc.nextInt(boundry - h_boundry) + h_boundry;
+			w = rc.nextInt(w_boundry) + 1;
+			h = rc.nextInt(h_boundry) + 1;
+		}
+	}
+}
+
+public class Problem1 {
+	public static void main(String[] args) throws IOException {
+		//change n to enlarge the size of data file
+		int n = 10000;
+		FileWriter pfw = new FileWriter("Point.txt");
+		FileWriter rfw = new FileWriter("Rec.txt");
+		for (int i = 0; i < n; i++) {
+			Data.points pnt = new Data.points();
+			pnt.create_point();
+			Data.rectangle rec = new Data.rectangle();
+			rec.create_rec();
+			String pt = "(" + pnt.x + "," + pnt.y + ")";
+			String rc = "<" + rec.bot_leftx + "," + rec.bot_lefty + "," + rec.w + "," + rec.h+">";
+			pfw.write(pt + "\n");
+			rfw.write(rc + "\n");
+		}
+		pfw.close();
+		rfw.close();
+	}
+
+}
